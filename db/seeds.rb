fc42e880fbdd01f5d1569f18beca74bc0da5caef
Lecogoni <giraud.nicolas@me.com>
2021-05-04 12:31:45 +0200

    scaffold articles - add  associciations between user & articles - add seed - PBM with seed no hit db

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,14 +12,39 @@ User.create(
   first_name: "marie",
   last_name: "angot",
   phone_number: "061234675678",
+  email: "marie@yopmail.com",
+  encrypted_password: "000000",
 )
+
+puts "marie create"
 
 User.create(
   first_name: "jacqueline",
   phone_number: "0622334455",
+  email: "jacqueline@yopmail.com",
+  encrypted_password: "000000",
 )
+
+puts "jacqueline create"
 
 User.create(
   first_name: "laurence",
   phone_number: "0766778899",
+  email: "laurence@yopmail.com",
+  encrypted_password: "000000",
 )
+
+puts "laurence create"
+
+article_title = ["fauteil louis XV", "Chaise vintage", "chaine de table", "tabouret", 
+  "chaise", "banquette", "banquette ancienne", "banquette ancienne", "chaise d'époque", 
+  "fauteuil club", "tabouret vintage" ]
+
+12.times do
+  Article.create(
+    title: article_title.sample,
+    user_id: 1
+  )
+end
+
+puts "12 article"
