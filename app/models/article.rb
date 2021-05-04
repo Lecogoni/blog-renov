0597b741118d69378c658af5f42eb2487adcd145
Lecogoni <giraud.nicolas@me.com>
2021-05-04 19:51:55 +0200

    navigation : articles#index, annuaire = users#index, articles#show

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,12 @@
 class Article < ApplicationRecord
   belongs_to :user
+
+  def upcase_title
+    title = self.title.upcase
+  end
+
+  def titleize_title
+    title = self.title.titleize
+  end
+
 end
