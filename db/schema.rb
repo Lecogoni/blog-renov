f78dbbfd81ed2d34b9fdc68a56dc2da2d355d94d
Lecogoni <giraud.nicolas@me.com>
2021-05-06 18:10:16 +0200

    add default value  to category > display

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2021_05_06_160150) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.boolean "display"
+    t.boolean "display", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
