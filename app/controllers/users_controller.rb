41e1aa79dc28903c319c69bb700e3f869211cd7a
Lecogoni <giraud.nicolas@me.com>
2021-06-08 16:32:00 +0200

    add column show_data & is_admin to users - add show_data condition to user#index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  include UsersHelper
+  
   before_action :set_user, only: %i[ show edit update destroy ]
 
   # GET /users or /users.json
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  def is_admin?
+    current_user.is_admin == true
+  end
+  
+  def show_data?
+    self.show_data == true
+  end
+
 end
