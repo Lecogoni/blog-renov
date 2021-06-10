cc4053fe2d1f626b5bf2fd90038f203b0e29a4b0
Lecogoni <giraud.nicolas@me.com>
2021-06-10 10:46:45 +0200

    user#edit check_box to display or not phone number and email in user#index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -80,6 +80,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :phone_number, :email, :avatar)
+      params.require(:user).permit(:first_name, :last_name, :phone_number, :email, :avatar, :show_data)
     end
 end
