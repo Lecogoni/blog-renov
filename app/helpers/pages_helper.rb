fd8e2fcc8e98f1c6d11f40f6eb776a101c80ae75
Lecogoni <giraud.nicolas@me.com>
2021-06-10 15:24:38 +0200

    add some stat to admin page

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -4,4 +4,17 @@ module PagesHelper
     Guest.all.count != 0
   end
 
+
+  def number_of_members
+    User.all.count
+  end
+
+  def number_of_articles
+    Article.all.count
+  end
+
+  def number_of_posts
+    Post.all.count
+  end
+
 end
