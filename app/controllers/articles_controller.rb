875fa6ccfad3d71f06b5fe7a97bbb73f51ab55cd
Lecogoni <giraud.nicolas@me.com>
2021-09-13 10:12:31 +0200

    WIP front on article#create form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[ show edit update destroy admin_delete_article]
   before_action :downcase_fields, only: %i[ create update ]
-  # Define first images as cover picture after create
   after_action :define_first_img_as_cover_img, only: %i[ create ]
   #after_action :picture_format, only: %i[ create update ]
   
@@ -52,11 +51,24 @@ class ArticlesController < ApplicationController
       if params[:article][:images].present?
         number_of_ActiveStorage_attachement = params[:article][:images].size
         @files = ActiveStorage::Attachment.where(record_id: @article.id, record_type: 'Article').last(number_of_ActiveStorage_attachement).reverse
-        picture_format(@files)
+        number_of_files_uploaded = @files.length
+        @images = upload_file_is_image?(@files)
+        number_of_images = @images.length
+        if number_of_images > 0
+          picture_format(@images)
+        end
       end
       
       redirect_to @article
-      flash[:success] = "Votre article a été enregistré !"
+      if number_of_images == 0
+        flash[:alert] = "Votre article a été enregistré, cependant les fichiers uploadés ne sont pas au format images. Ils n'apparaîtrons pas"
+      elsif number_of_files_uploaded - number_of_images == 1 
+        flash[:alert] = "Votre article a été enregistré, cependant 1 fichier uploadé n'est pas au format image. Il n'apparaîtra pas"
+      elsif number_of_files_uploaded - number_of_images > 1
+        flash[:alert] = "Votre article a été enregistré, cependant #{number_of_files_uploaded - number_of_images}  fichiers uploadés ne sont pas au format image. Ils n'apparaîtront pas"
+      else
+        flash[:success] = "Votre article a été enregistré !"
+      end
     else
       render :new, status: :unprocessable_entity
       flash[:error] = "votre publication n'a pas été enregistrée"
@@ -82,30 +94,57 @@ class ArticlesController < ApplicationController
     
     if @article.update(article_params)
 
+      number_of_img_uploaded = 0
       # check if there is ActiveStorage Attachement and if yes get them in @files which is then send to a method to delete files != image, and resize image
+
       if params[:article][:images].present?
         number_of_ActiveStorage_attachement = params[:article][:images].size
         @files = ActiveStorage::Attachment.where(record_id: @article.id, record_type: 'Article').last(number_of_ActiveStorage_attachement).reverse
+      
+        #puts "------******************************----------- ------******************************----------- ------******************************-----------"
+        # puts "------******************************----------- ------******************************----------- ------******************************-----------"
+        # puts "@files : #{@files}" 
+        # puts "@files.class : #{@files.class}"
+        # puts "------FILES SIZE BEFORE ERASE-----------"
+    
+ 
+
+        # erased no image
+
+
+        # puts "------FILE SIZE AFTER ERASE-----------"
+
+        numbFiles = @files.length.to_i
+        @images = upload_file_is_image?(@files)
+        numbImg = @images.length.to_i
+        #puts "number_of_images =  : #{numbFiles}"
+        number_of_img_uploaded = numbFiles - numbImg
+        puts "variable = #{number_of_img_uploaded}"
+        # puts "number_of_images : #{number_of_images}"
+        #picture_format(@images)
+        if @images.length > 0
+          picture_format(@images)
+        end
       end
-
-      puts "------******************************----------- ------******************************----------- ------******************************-----------"
-      puts @files
-      puts @files.class
-
-      puts "------******************************----------- ------******************************----------- ------******************************-----------"
-      puts "------FILES SIZE BEFORE ERASE-----------"
-      number_of_files_uploaded = @files.count
-      number_of_files_uploaded = @files.length
-      puts number_of_files_uploaded
-      @images = upload_file_is_image?(@files)
-      puts "------******************************----------- ------******************************----------- ------******************************-----------"
-      puts "------FILE SIZE AFTER ERASE-----------"
-      number_of_images = @images.count
-      puts number_of_images
-      picture_format(@images)
+      
+      puts "variable = #{number_of_img_uploaded}"
 
       redirect_to @article
-      flash[:success] = "Votre article est bien modifié !"
+      
+      if !params[:article][:images].present?
+        flash[:success] = "Votre article est bien modifié !"
+      elsif numbFiles === numbImg
+        flash[:success] = "Votre article est bien modifié !"
+      elsif numbFiles != numbImg
+
+        if number_of_img_uploaded == 1
+          flash[:alert] = "Votre article a été modifié, cependant 1 fichier uploadé n'est pas au format image. Il n'apparaîtra pas"
+        elsif number_of_img_uploaded > 1
+          flash[:alert] = "Votre article a été modifié, cependant #{numbFiles - numbImg} fichiers uploadés ne sont pas au format images. Ils n'apparaîtrons pas"
+        end
+
+      end
+
     else
       render :edit, status: :unprocessable_entity
     end
@@ -160,33 +199,93 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # check if blob is an image, if not delete the blob and send a message 
-  # then if the image isn't a variable? it convert to jpeg
+  # check if an uploaded file is a image - if not send to be purge - count the number of files purged
+  def upload_file_is_image?(files)
+    files.each do |file|
+      if !file.content_type.start_with? 'image/'
+        purge_file(file)
+        files.delete file
+      end
+    end
+    return files
+  end
+
+  # purge actoive storage attachement
+  def purge_file(file)
+    file.purge_later
+  end
+
+
+  # if image blob isn't representable convert in appropriate format
+  # avoid to get HEIC for e.g. - impossible to get a variant on content_type not true
+  def image_blob_is_variable?(images)
+    images.each do |image|
+      if !image.variable?
+        puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+        puts "IMAGE NOT VARIABLE"
+        puts image.variable?
+        blob = image.blob
+        puts image.filename
+        image.filename = "image"
+        puts image.filename
+      end
+
+    end
+    return images
+  end
+
+  # Resize image which whdth or height is superior or equal to my resize_size
   def picture_format(images)
+    resize_size = 900
     puts "------******************************-----------"
     puts "------IN PICTURES FORMAT-----------"
-    images.each do |image|
-    
-        # file is a ActiveStorage::Attachment we convert in ActiveStorage::Blob
-        blob = image.blob
-        puts "------*****************IMAGE *************-----------"
-        puts image
-        puts "-----BLOB-----------"
-        puts blob.variable?
-        puts blob
 
-        blob.open do |picture|
-          puts "------PICTURES PATH -----------"
-          path = picture.path
-          puts picture.path
-          ImageProcessing::MiniMagick.source(path)
-            .resize_to_limit(1200, 1200)
-            .call(destination: path)
-          new_data = File.binread(path)
-          @article.send(:images).attach io: StringIO.new(new_data), filename: blob.filename.to_s, content_type: 'image'
-          puts "----------------------------------"
+    #puts "images.length #{images.length}"
+    #puts "images.class #{images.class}"
+
+    #puts "------        -----------"
+
+      images.each do |image|
+        meta = ActiveStorage::Analyzer::ImageAnalyzer.new(image).metadata
+        #puts "image : #{image}"
+        #puts "image.class : #{image.class}"
+        if meta[:width] >= resize_size || meta[:height] >= resize_size   
+          blob = image.blob
+          #puts "blob : #{blob}"
+          #puts "blob.class : #{blob.class}"
+          blob.open do |picture|
+            #puts pic
+            #puts pic.class
+            #puts pic.path
+
+
+            ImageProcessing::MiniMagick.source(picture.path)
+              .resize_to_limit(resize_size, resize_size)
+              .quality(80)
+              .call(destination: picture.path) #picture seulement marche aussi
+            new_data = File.binread(picture.path)
+            @article.send(:images).attach io: StringIO.new(new_data), filename: blob.filename.to_s, content_type: 'image'
+            image.purge
+          end
         end
-        image.purge_later 
+
+      end
+    
+
+
+
+        # blob.open do |picture|
+        #   puts "------PICTURES PATH -----------"
+        #   path = picture.path
+        #   puts picture.path
+        #   ImageProcessing::MiniMagick.source(path)
+        #     .resize_to_limit(1200, 1200)
+        #     .call(destination: path)
+        #   new_data = File.binread(path)
+        #   @article.send(:images).attach io: StringIO.new(new_data), filename: blob.filename.to_s, content_type: 'image'
+        #   puts "----------------------------------"
+        # end
+        #image.purge_later 
 
         # blob.open do |temp_file| # temp_file
         #   path = temp_file.path #temp_file
@@ -207,39 +306,12 @@ class ArticlesController < ApplicationController
         # end
 
         # image.purge #purge_later 
-    end
+    #end
 
   end
 
 
-  # check if an uploaded file is a image - if not send to be purge - count the number of files purged
-  def upload_file_is_image?(files)
-    files.each do |file|
-      if !file.content_type.start_with? 'image/'
-        #purge_file(file)
-        file.purge
-        files.delete file
-      end
-    end
-    puts "------******************************-----------"
-    puts "------******************************-----------"
-    puts files.size
-    files.each do |file|
-      puts file
-    end
 
-    return files
-  end
-
-  def purge_file(file)
-    file.purge
-  end
-
-  # if image blob isn't representable convert in appropriate format
-  # avoid to get HEIC for e.g. - impossible to get a variant on content_type not true
-  def image_blob_is_variable?(image)
-    image.variable?
-  end
 
 end
 
